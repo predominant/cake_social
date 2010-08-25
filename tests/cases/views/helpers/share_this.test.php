9c9b093cfc1fe4bf3ce6e1b502baa7d004d1ef83
predominant <graham@grahamweldon.com>
2010-08-26 02:34:57 +1000

    Added ShareThis helper.

---
+++ tests/cases/views/helpers/share_this.test.php
@@ -0,0 +1,176 @@
+<?php
+
+App::import('Helper', array('CakeSocial.ShareThis', 'Html'));
+
+/**
+ * TheJsTestController class
+ *
+ * @package       cake_social
+ * @subpackage    cake_social.tests.views.helpers
+ */
+class TheJsTestController extends Controller {
+
+/**
+ * name property
+ *
+ * @var string 'TheTest'
+ */
+	public $name = 'TheTest';
+
+/**
+ * uses property
+ *
+ * @var mixed null
+ */
+	public $uses = null;
+}
+
+/**
+ * TheView class
+ *
+ * @package       cake_social
+ * @subpackage    cake_social.tests.views.helpers
+ */
+class TheView extends View {
+
+/**
+ * scripts method
+ *
+ * @return void
+ */
+	public function scripts() {
+		return $this->__scripts;
+	}
+}
+
+/**
+ * ShareThis Test Case
+ *
+ * @package cake_social
+ * @subpackage cake_social.tests.views.helpers
+ * @author Graham Weldon (http://grahamweldon.com)
+ */
+class ShareThisHelperTestCase extends CakeTestCase {
+
+/**
+ * Start Test
+ *
+ * Setup class vars for testing
+ *
+ * @return void
+ */
+	public function startTest() {
+		$this->ShareThis = new ShareThisHelper();
+		$this->ShareThis->Html = new HtmlHelper();
+		$this->View =& new TheView(new TheJsTestController());
+		ClassRegistry::addObject('view', $this->View);
+	}
+
+/**
+ * End Test
+ *
+ * Clean up after each test is run
+ *
+ * @return void
+ */
+	public function endTest() {
+		unset($this->ShareThis);
+		ClassRegistry::removeObject('view');
+		unset($this->View);
+	}
+
+/**
+ * testSocialType
+ *
+ * @return void
+ */
+	public function testSocialType() {
+		$expected = '<span class="st_test"></span>';
+		$result = $this->ShareThis->socialType('test');
+		$this->assertIdentical($expected, $result);
+	}
+
+/**
+ * testScripts
+ *
+ * @return void
+ */
+	public function testScripts() {
+		$this->ShareThis->display();
+		$result = $this->View->scripts();
+		foreach ($result as &$script) {
+			$script = str_replace("\n", '', $script);
+		}
+		$this->assertIdentical(
+			$result,
+			array(
+				'<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>',
+				'<script type="text/javascript">//<![CDATA[stLight.options({publisher:\'\'});//]]></script>'
+			)
+		);
+	}
+
+/**
+ * testPublisherScripts
+ *
+ * @return void
+ */
+	public function testPublisherScripts() {
+		$this->ShareThis->display(array(), array('publisher' => 'Mr. Man'));
+		$result = $this->View->scripts();
+		foreach ($result as &$script) {
+			$script = str_replace("\n", '', $script);
+		}
+		$this->assertIdentical(
+			$result,
+			array(
+				'<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>',
+				'<script type="text/javascript">//<![CDATA[stLight.options({publisher:\'Mr. Man\'});//]]></script>'
+			)
+		);
+	}
+
+/**
+ * testDefault
+ *
+ * @return void
+ */
+	public function testDefault() {
+		$expected = '<span class="st_twitter"></span><span class="st_facebook"></span><span class="st_ybuzz"></span><span class="st_gbuzz"></span><span class="st_email"></span><span class="st_sharethis"></span>';
+		$result = $this->ShareThis->display();
+		$this->assertIdentical($expected, $result);
+	}
+
+/**
+ * testSingle
+ *
+ * @return void
+ */
+	public function testSingle() {
+		$expected = '<span class="st_twitter"></span><span class="st_sharethis"></span>';
+		$result = $this->ShareThis->display(array('twitter'));
+		$this->assertIdentical($expected, $result);
+	}
+
+/**
+ * testNoShareThis
+ *
+ * @return void
+ */
+	public function testNoShareThis() {
+		$expected = '<span class="st_twitter"></span><span class="st_facebook"></span><span class="st_ybuzz"></span><span class="st_gbuzz"></span><span class="st_email"></span>';
+		$result = $this->ShareThis->display(array(), array('sharethis' => false));
+		$this->assertIdentical($expected, $result);
+	}
+
+/**
+ * testNonArrayTypes
+ *
+ * @return void
+ */
+	public function testNonArrayTypes() {
+		$expected = '<span class="st_sharethis"></span>';
+		$result = $this->ShareThis->display(null);
+		$this->assertIdentical($expected, $result);
+	}
+}
---
+++ views/helpers/share_this.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * ShareThis Helper
+ *
+ * Useful view helper for the ShareThis service
+ *
+ * @package default
+ * @subpackage cake_social.views.helpers
+ * @author Graham Weldon (http://grahamweldon.com)
+ */
+class ShareThisHelper extends AppHelper {
+
+/**
+ * View helpers
+ *
+ * @var array
+ */
+	public $helpers = array('Html');
+
+/**
+ * Default Social Types
+ *
+ * @var array
+ */
+	protected $_defaultTypes = array(
+		'twitter',
+		'facebook',
+		'ybuzz',
+		'gbuzz',
+		'email',
+	);
+
+/**
+ * Social Media Types
+ *
+ * @var array
+ */
+	protected $_types = array(
+		'aim',
+		'bebo', 'blinklist', 'blogger', 'businessweek',
+		'care2', 'current',
+		'dealsplus', 'delicious', 'digg', 'dilgo',
+		'facebook', 'fark', 'faves', 'fresqui', 'friendfeed', 'funp',
+		'google_bmarks',
+		'kirsty',
+		'linkedin',
+		'meaneame', 'messenger', 'mister_wong', 'mixx', 'myspace',
+		'n4g', 'newsvine',
+		'oknotizie',
+		'propeller',
+		'reddit',
+		'simpy', 'slashdot', 'sonico', 'sphinn', 'stumbleupon',
+		'technorati', 'twackle', 'twine', 'twitter',
+		'windows_live',
+		'xanga',
+		'yahoo_bmarks', 'ybuzz', 'yigg',
+	);
+
+/**
+ * Default options
+ *
+ * ### Options
+ *
+ * - `sharethis` (boolean) Include the 'sharethis' button with the social type set. Default: true
+ * - `publisher` (string) Your publisher ID obtained from [ShareThis](http://sharethis.com)
+ * - `buttonJs` (string) URL to the Javascript for the ShareThis button
+ *
+ * @var array
+ */
+	protected $_options = array(
+		'sharethis' => true,
+		'publisher' => '',
+		'buttonJs' => 'http://w.sharethis.com/button/buttons.js',
+	);
+
+/**
+ * Constructor
+ *
+ * Takes options to configure instance variables like publisher
+ *
+ * ### Options
+ *
+ * - `publisher` (string) Publisher key from [ShareThis](http://sharethis.com)
+ *
+ * @param array $options 
+ */
+	public function __construct($options = array()) {
+		$this->_options = array_merge($this->_options, $options);
+	}
+
+/**
+ * Display the ShareThis widget
+ *
+ * @param array $types Social Media types (See ShareThisHelper::$_types)
+ * @param array $options Display options (See ShareThisHelper::$_options)
+ * @return string Html for the ShareThis widget
+ */
+	public function display($types = array(), $options = array()) {
+		if (is_array($types)) {
+			if (empty($types)) {
+				$types = $this->_defaultTypes;
+			}
+		} else {
+			$types = array();
+		}
+
+		$options = array_merge($this->_options, $options);
+		$result = '';
+		foreach ($types as $type) {
+			$result .= $this->socialType($type);
+		}
+		if ($options['sharethis']) {
+			$result .= $this->socialType('sharethis');
+		}
+		$this->_scripts($options);
+		return $result;
+	}
+
+/**
+ * Generate a single social type span element
+ *
+ * @param string $type Social type key (See ShareThisHelper::$_types)
+ * @return string Social Type HTML
+ */
+	public function socialType($type) {
+		return $this->Html->tag('span', '', 'st_' . $type);
+	}
+
+/**
+ * Generate required Javascript
+ *
+ * @param array $options Options
+ * @return void
+ */
+	public function _scripts($options = array()) {
+		$options = array_merge($this->_options, $options);
+		$this->Html->script($options['buttonJs'], array('inline' => false));
+		$this->Html->ScriptBlock(sprintf(
+			'stLight.options({publisher:\'%s\'});', $options['publisher']
+		), array('inline' => false));
+	}
+}
